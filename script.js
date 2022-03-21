fb73be9a9601dbb8bb6ca8605c46861f0a324565
TomKrok1 <91782249+trak2022z@users.noreply.github.com>
2022-03-21 18:45:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+(function() {
+window.addEventListener("load", init);
+// no access to the document here
+function init() {
+  
+  // create a new <h2> node
+  let newHeading = document.createElement("h2");
+  newHeading.textContent = "This is a new heading!";
+  newHeading.id = 'content';
+  document.body.appendChild(newHeading);
+}
+
+  let li = document.createElement("li");
+  li.textContent = "Contact!";
+  
+  // select the ul menu element
+  const menu = document.querySelector('#menu');
+  menu.appendChild(li);
+
+  //Remove element by id
+  let el = document.getElementById("fyi");
+  el.innerHTML = "";
+  
+})();
+
+
+
